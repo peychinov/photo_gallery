d51d6747e8a79e48c79efd4b55f7e6d54cb2b156
Dimitar Peychinov <dp86jd@gmail.com>
2013-06-03 15:22:41 +0300

    Run migrations for last commits.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,37 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130603101806) do
+ActiveRecord::Schema.define(:version => 20130603121756) do
+
+  create_table "categories", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "comments", :force => true do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.integer  "photo_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "comments", ["photo_id"], :name => "index_comments_on_photo_id"
+  add_index "comments", ["user_id"], :name => "index_comments_on_user_id"
+
+  create_table "photos", :force => true do |t|
+    t.string   "title"
+    t.string   "image"
+    t.integer  "user_id"
+    t.integer  "category_id"
+    t.boolean  "comments_allowed"
+    t.datetime "created_at",       :null => false
+    t.datetime "updated_at",       :null => false
+  end
+
+  add_index "photos", ["category_id"], :name => "index_photos_on_category_id"
+  add_index "photos", ["user_id"], :name => "index_photos_on_user_id"
 
   create_table "users", :force => true do |t|
     t.string   "first_name"
