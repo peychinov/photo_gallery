7da2e819dd709a220ffb7653fd805489b5356a58
Dimitar Peychinov <dp86jd@gmail.com>
2013-06-03 22:00:56 +0300

    Changes to the images.

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -29,7 +29,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process :resize_to_fit => [800, 800]
+  process :resize_to_fit => [500, 500]
 
   #
   # def scale(width, height)
