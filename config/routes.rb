84cd1a4971f9a09b29ab6fea5a61ca9087bb74b1
Dimitar Peychinov <dp86jd@gmail.com>
2013-06-03 13:13:26 +0300

    Configured devise.

--- config/routes.rb
+++ config/routes.rb
@@ -48,7 +48,7 @@ PhotoGallery::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => '/'
+  root :to => 'photos#index'
 
   # See how all your routes lay out with "rake routes"
 
