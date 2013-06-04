9bf699456f18c47d15e4bc591c02a1c71fabd4c9
Dimitar Peychinov <dp86jd@gmail.com>
2013-06-04 10:51:42 +0300

    CHanges related mostly to categories.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -14,6 +14,7 @@ class CategoriesController < ApplicationController
   # GET /categories/1.json
   def show
     @category = Category.find(params[:id])
+    @photos = Photo.where(category_id: @category)
 
     respond_to do |format|
       format.html # show.html.erb
